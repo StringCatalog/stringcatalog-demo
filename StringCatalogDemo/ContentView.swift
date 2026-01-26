2691e1772f2e86dd92379a6245c51d97520d39c5
Bill Richards <roundedvision@gmail.com>
2026-01-26 10:04:36 -0800

    small change test

--- StringCatalogDemo/ContentView.swift
+++ StringCatalogDemo/ContentView.swift
@@ -62,7 +62,7 @@ struct ContentView: View {
                 Button(action: {
                     showIntro = true
                 }) {
-                    Text("Learn More")
+                    Text("Learn More!")
                         .fontWeight(.semibold)
                         .foregroundColor(.white)
                         .frame(maxWidth: .infinity)
